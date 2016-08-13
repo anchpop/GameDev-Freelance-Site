f94496aa1db2533d84e1e7efec3312a08bc1a695
Andre Popovitch <andre@andreisawesome.com>
2016-08-13 12:01:57 -0400

    started using process.env.PWD instead of __dirname

--- app.js
+++ app.js
@@ -10,25 +10,27 @@ var users = require('./routes/users');
 
 var app = express();
 
+process.env.PWD = process.cwd();
+
 //require('dotenv').config()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(process.env.PWD, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(process.env.PWD, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  src: path.join(process.env.PWD, 'public'),
+  dest: path.join(process.env.PWD, 'public'),
   indentedSyntax: true,
   sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(process.env.PWD, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
