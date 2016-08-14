6dbc9de02b818169dd9ac4552ff416832af25a09
Andre Popovitch <andre@andreisawesome.com>
2016-08-14 17:09:16 -0400

    normalize directory?

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(require('node-sass-middleware')({
   indentedSyntax: true,
   sourceMap: true
 }));
-app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use(express.static(path.normalize(path.join(process.env.PWD, 'public'))));
 
 app.use('/', routes);
 app.use('/users', users);
